6de56ee299d533dcf38777ecf3835f458dfa4147
Guille47 <62317494+Guille47@users.noreply.github.com>
2021-11-13 11:00:09 -0600

    Agregar archivos de proyecto.

---
+++ KendoMVC/Controllers/ExamenController.cs
@@ -0,0 +1,102 @@
+﻿using KendoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoMVC.Controllers
+{
+    public class ExamenController : Controller
+    {
+        // GET: Examen
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult IndexGetData()
+        {
+            List<AreaViewModel> lst = null;
+            using (ExamenEntities db = new ExamenEntities())
+            {
+                lst = (from d in db.Areas
+                       orderby d.IdArea
+                       select new AreaViewModel
+                       {
+                           IdArea = d.IdArea,
+                           Nombre = d.Nombre,
+                           Descripcion = d.Descripcion
+                       }).ToList();
+            }
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Agregar(AddAreaViewModel model)
+        {
+            int response = -1;
+            try
+            {
+                using (var db = new ExamenEntities())
+                {
+                    Area oAreas = new Area();
+                    oAreas.Nombre = model.Nombre;
+                    oAreas.Descripcion = model.Descripcion;
+                    db.Areas.Add(oAreas);
+                    db.SaveChanges();
+                    response = oAreas.IdArea;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return Content(response.ToString());
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EditAreaViewModel model)
+        {
+            int response = -1;
+            try
+            {
+                using (var db = new ExamenEntities())
+                {
+                    Area oAreas = db.Areas.Find(model.IdArea);
+                    oAreas.Nombre = model.Nombre;
+                    oAreas.Descripcion = model.Descripcion;
+                    db.Entry(oAreas).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    response = oAreas.IdArea;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return Content(response.ToString());
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(int Id)
+        {
+            int response = -1;
+            try
+            {
+                using (var db = new ExamenEntities())
+                {
+                    Area oAreas = db.Areas.Find(Id);
+                    db.Areas.Remove(oAreas);
+                    db.SaveChanges();
+                    response = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Content(response.ToString());
+        }
+
+    }
+}
---
+++ KendoMVC/Models/AreaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoMVC.Models
+{
+    public class AreaViewModel
+    {
+        public int IdArea { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+    public class AddAreaViewModel
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+    public class EditAreaViewModel
+    {
+        public int IdArea { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
